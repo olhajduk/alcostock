3978b191e4049e4e0235d2d6ec7788beceffb220
Olga Hajduk <hajduk.o.a@gmail.com>
2020-08-19 20:54:08 +0300

    add header, slick

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './scss/index.scss';
-
+import $ from 'jquery';
+import slick from 'slick-carousel';
 console.log('Working!');
+
+$('.slick').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
