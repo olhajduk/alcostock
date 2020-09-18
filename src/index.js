c7a590682134ff3a8c6baad6e50dcec2aabf46dc
Olga Hajduk <hajduk.o.a@gmail.com>
2020-09-18 16:02:04 +0300

    fix webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import './scss/index.scss';
-import $ from 'jquery';
-import slick from 'slick-carousel';
 console.log('Working!');
-
-$('.slick').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1
-  });
