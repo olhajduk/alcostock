b57f7585ad218d43b9a3e74dc0347e6713058db9
Olga Hajduk <hajduk.o.a@gmail.com>
2020-09-20 20:27:31 +0300

    add swiper for brands

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import Splide from '@splidejs/splide';
 import './scss/index.scss';
 
-// slider
-new Splide( '#splide' ).mount();
-new Splide( '#splide', {
-    type   : 'loop',
-    perPage: 3,
-  } );
 
-
-console.log('Working!');
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('Working!');
+  });
