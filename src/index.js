104bb9bae85be3a64b17440bd4eca4a8fc049b4b
Olga Hajduk <hajduk.o.a@gmail.com>
2020-09-20 15:25:40 +0300

    add splidejs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+import Splide from '@splidejs/splide';
 import './scss/index.scss';
+
+// slider
+new Splide( '#splide' ).mount();
+new Splide( '#splide', {
+    type   : 'loop',
+    perPage: 3,
+  } );
+
+
 console.log('Working!');
