146661c95b545f22618a2dc2783ecb7590b73c04
Olga Hajduk <hajduk.o.a@gmail.com>
2020-10-03 19:12:45 +0300

    optimization: add lazy load for img in slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    let swiper = new Swiper('.swiper-container', {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-          let arr = new Array('виски', 'коньяк','ром','арманьяк','вино','шампанское');
-          return '<span class="' + className + ' button ">' +  (arr[index])  + '</span>';
-        },
+  let swiper = new Swiper('.swiper-container', {
+    preloadImages: false,
+    lazy: {
+      loadPrevNext: true,
+    },
+    watchSlidesProgress:true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        let arr = new Array('виски', 'коньяк', 'ром', 'арманьяк', 'вино', 'шампанское');
+        return '<span class="' + className + ' button ">' + (arr[index]) + '</span>';
       },
-    });
-  
-    console.log('Working!');
+    },
   });
+
+  console.log('Working!');
+});
