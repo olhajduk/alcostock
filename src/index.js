71f09c7e99f43371896e0ff8b8c6f00ead0dbff0
Olga Hajduk <hajduk.o.a@gmail.com>
2020-10-07 21:32:46 +0300

    add load and swiper npm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './scss/index.scss';
-import './js/swiper';
 import './js/brands';
+
+import lozad from 'lozad';
+const observer = lozad();
+observer.observe();
+
+import Swiper from './../node_modules/swiper/swiper-bundle.min';
+
+const swiper = new Swiper('.swiper-container', {
+    preloadImages: false,
+    lazy: {
+        loadPrevNext: true,
+    },
+    watchSlidesProgress: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            let arr = new Array('виски', 'коньяк', 'ром', 'арманьяк', 'вино', 'шампанское');
+            return '<span class="' + className + ' button ">' + (arr[index]) + '</span>';
+        },
+    },
+});
+
+
+
